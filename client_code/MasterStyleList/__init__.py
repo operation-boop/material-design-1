628be79a44193e0f6cd8fbba67360e1993e39aea
operation@vinmake.com <operation@vinmake.com>
2025-11-04 08:03:58 +0000

    Edited MasterStyleList

--- client_code/MasterStyleList/__init__.py
+++ client_code/MasterStyleList/__init__.py
@@ -3,11 +3,11 @@ from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from datetime import datetime
+
 
 class MasterStyleList(MasterStyleListTemplate):
-  """Main list view for Master Styles"""
-
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
